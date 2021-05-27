8017b378336588d1dffcdbcf8b8153d623d8556f
Ilmir <78535469+root0f@users.noreply.github.com>
2021-05-27 09:57:23 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from "@material-ui/core/Button";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button>123</Button>
+    </div>
+  );
+}
+
+export default App;
